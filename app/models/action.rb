1326252791d5521ae30a2a2f90bae1a06920f72c
berlin <ilya.kolodnik@gmail.com>
2014-05-01 09:50:05 +0300

    remove unused classes

--- app/models/action.rb
+++
@@ -1,5 +0,0 @@
-class Action < Spira::Base
-  configure default_vocabulary: RDF::URI.new('http://example.org/vocab'),
-            base_uri: "http://exampe.org/actions"
-  property :name, type: String
-end
--- config/initializers/spira_repository.rb
+++
@@ -1,2 +0,0 @@
-require 'rdf/sesame'
-Spira.repository = RDF::Sesame::Repository.new('http://localhost:8080/openrdf-sesame/repositories/1')

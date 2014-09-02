fd5dc93c9b2f6f765b1838b77248b47d4a816c0d
berlin <ilya.kolodnik@gmail.com>
2014-09-02 12:11:08 +0300

    add touch

--- app/models/day.rb
+++ app/models/day.rb
@@ -14,7 +14,7 @@ class Day < ActiveRecord::Base
 
   DAY_NAMES = [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday]
 
-  belongs_to :group
+  belongs_to :group, touch: true
   has_many :lessons
   accepts_nested_attributes_for :lessons, allow_destroy: true
 
